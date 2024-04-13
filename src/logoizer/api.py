02acea9a86a2202cdf3901fc96adf6aa0cb2b81a
mcflugen <mcflugen@gmail.com>
2024-04-12 20:20:56 -0600

    use importlib to get the locations of fonts

--- src/logoizer/api.py
+++ src/logoizer/api.py
@@ -1,24 +1,32 @@
 from __future__ import annotations
 
-import pathlib
+import os
+import sys
+from glob import glob
 from io import BytesIO
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
+if sys.version_info >= (3, 12):  # pragma: no cover (PY12+)
+    import importlib.resources as importlib_resources
+else:  # pragma: no cover (<PY312)
+    import importlib_resources
 
-def load_logo_font(family: str = "Quicksand") -> None:
-    this_folder = pathlib.Path(__file__).parent
-    path_to_fonts = this_folder / "fonts" / family / "static"
-    for font in path_to_fonts.glob("*.ttf"):
+
+def _load_logo_font(family: str = "Quicksand") -> None:
+    path_to_fonts = str(
+        importlib_resources.files("logoizer") / "fonts" / family / "static"
+    )
+    for font in glob(os.path.join(path_to_fonts, "*.ttf")):
         font_manager.fontManager.addfont(str(font))
     mpl.rc("font", family=family)
 
 
 def logoize(words: str, dest: BytesIO, light: bool = True, format: str = "svg") -> None:
     color = (0.0, 0.0, 0.0) if light else (1.0, 1.0, 1.0)
-    load_logo_font()
+    _load_logo_font()
 
     plt.figtext(
         0.5,
